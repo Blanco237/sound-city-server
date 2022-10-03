e9a9abd9a506b47eb16a8ef05fcb05c4ab51a55e
Blanco237 <asongrandy9@gmail.com>
2022-10-03 11:22:50 +0100

    Secure URLS

--- routes/Upload.js
+++ routes/Upload.js
@@ -8,7 +8,8 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
 });
 
 router.post('/image', async (req, res) => {
